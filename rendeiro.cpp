95fecee092b4e35097aafd3788e1e7dd96166cf1
RendeiroDavi <rendeiroodavi@gmail.com>
2025-04-04 11:22:29 -0300

    Programa 1

---
+++ rendeiro.cpp
@@ -0,0 +1,14 @@
+//programando "ola mundo!" 2.0
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+    printf("ola mundo!\n");
+    std::cout << "ola mundo!" << std::endl;
+    return 0;
+
+}
+// "sid::enl" = fim de linha = proxima linha = "\n"
+// "g++" = salva compilador de e.g. (de .cpp para .exe)
+// ".\ARQUIVO.exe" = executa o "ARQUIVO"
+
